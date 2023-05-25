7739f08e9addd7f00eef6213988c2539da93a639
vasiliyantufev <4541435+vasiliyantufev@users.noreply.github.com>
2023-05-25 15:01:28 +0300

    grpc client init

--- cmd/gophkeeper/client/main.go
+++ cmd/gophkeeper/client/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"context"
+
 	"github.com/sirupsen/logrus"
 	"github.com/vasiliyantufev/gophkeeper/internal/config/configagent"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	grpcClient "github.com/vasiliyantufev/gophkeeper/internal/proto"
 )
 
 func main() {
 	log := logrus.New()
 	config := configagent.NewConfigClient(log)
 	log.SetLevel(config.DebugLevel)
+
+	conn, err := grpc.Dial(config.GRPC, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := grpcClient.NewGophkeeperClient(conn)
+	resp, err := client.HandlePing(context.Background(), &grpcClient.PingRequest{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Info(resp)
 }
