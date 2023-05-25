3d7661d60b376be8e8affb4df0de99fe6e909248
vasiliyantufev <4541435+vasiliyantufev@users.noreply.github.com>
2023-05-25 18:51:01 +0300

    session key

--- migrations/000005_metadata.up.sql
+++ migrations/000005_metadata.up.sql
@@ -1,9 +1,9 @@
 create table metadata
 (
-    metadata_id      serial PRIMARY KEY,
-    cart_name        varchar(50) NOT NULL,
-    cart_description varchar(300) NULL,
-    created_at       timestamp   NOT NULL,
-    updated_at       timestamp   NOT NULL,
-    deleted_at       timestamp NULL
+    metadata_id serial PRIMARY KEY,
+    name        varchar(50) NOT NULL,
+    description varchar(300) NULL,
+    created_at  timestamp   NOT NULL,
+    updated_at  timestamp   NOT NULL,
+    deleted_at  timestamp NULL
 )
--- migrations/000006_session.up.sql
+++ migrations/000006_session.up.sql
@@ -4,5 +4,5 @@ create table session
     user_id          int         NOT NULL references users (user_id) on delete cascade,
     session_key      varchar(30) NOT NULL,
     created_at       timestamp   NOT NULL,
-    created_at       timestamp   NOT NULL,
+    end_date_at       timestamp   NOT NULL,
 )
