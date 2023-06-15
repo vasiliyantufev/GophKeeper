8fc8f50d82e1b33b13583a612af907cc8f1f2c3a
vasiliyantufev <4541435+vasiliyantufev@users.noreply.github.com>
2023-06-15 16:22:57 +0300

    Synchronization, metadata

---
+++ internal/server/storage/variables/metadata.go
@@ -0,0 +1,8 @@
+package consts
+
+type Metadata string
+
+const (
+	Name        Metadata = "Name"
+	Description Metadata = "Description"
+)
--- internal/server/storage/variables/type.go
+++ internal/server/storage/variables/type.go
@@ -1,4 +1,4 @@
-package types
+package consts
 
 type Types string
 
