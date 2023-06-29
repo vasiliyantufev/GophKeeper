cec130fc87f2878bc7b2b1e2534e2b9c680eb3d0
vasiliyantufev <4541435+vasiliyantufev@users.noreply.github.com>
2023-06-29 11:39:32 +0300

    EventDeleteLoginPassword

--- internal/client/api/events/event_delete_login_password.go
+++ internal/client/api/events/event_delete_login_password.go
@@ -1,14 +1,26 @@
 package events
 
 import (
+	"context"
+
 	"github.com/sirupsen/logrus"
 	"github.com/vasiliyantufev/gophkeeper/internal/client/model"
+	grpc "github.com/vasiliyantufev/gophkeeper/internal/server/proto"
+	"github.com/vasiliyantufev/gophkeeper/internal/server/service"
 )
 
-func (c Event) EventDeleteLoginPassword(row []string, token model.Token) error {
+func (c Event) EventDeleteLoginPassword(loginPassword []string, token model.Token) error {
 	c.logger.Info("Delete login password")
 
-	logrus.Info(row)
-
+	created, _ := service.ConvertTimeToTimestamp(token.CreatedAt)
+	endDate, _ := service.ConvertTimeToTimestamp(token.EndDateAt)
+	deletedText, err := c.grpc.HandleDeleteLoginPassword(context.Background(),
+		&grpc.DeleteLoginPasswordRequest{Name: loginPassword[0], AccessToken: &grpc.Token{Token: token.AccessToken, UserId: token.UserID, CreatedAt: created, EndDateAt: endDate}})
+	if err != nil {
+		c.logger.Error(err)
+		return err
+	}
+	logrus.Info(loginPassword)
+	c.logger.Debug(deletedText)
 	return nil
 }
