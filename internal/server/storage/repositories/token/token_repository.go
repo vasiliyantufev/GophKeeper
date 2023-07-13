b6525b6c91c316c322d35441847b532daf88cf39
vasiliyantufev <4541435+vasiliyantufev@users.noreply.github.com>
2023-07-14 00:41:59 +0300

    user_repository, token_repository

--- internal/server/storage/repositories/token/token_repository.go
+++ internal/server/storage/repositories/token/token_repository.go
@@ -1,6 +1,7 @@
 package token
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/vasiliyantufev/gophkeeper/internal/client/service/encryption"
@@ -8,6 +9,7 @@ import (
 	"github.com/vasiliyantufev/gophkeeper/internal/server/model"
 	grpc "github.com/vasiliyantufev/gophkeeper/internal/server/proto"
 	"github.com/vasiliyantufev/gophkeeper/internal/server/service"
+	"github.com/vasiliyantufev/gophkeeper/internal/server/storage/errors"
 )
 
 const lengthToken = 32
@@ -26,11 +28,10 @@ func New(db *database.DB) *Token {
 	}
 }
 
-func (t Token) Create(userID int64, lifetime time.Duration) (*model.Token, error) {
+func (t *Token) Create(userID int64, lifetime time.Duration) (*model.Token, error) {
 	token := &model.Token{}
 	accessToken := encryption.GenerateAccessToken(lengthToken)
 	currentTime := time.Now()
-
 	if err := t.db.Pool.QueryRow(
 		"INSERT INTO access_token (access_token, user_id, created_at, end_date_at) VALUES ($1, $2, $3, $4) RETURNING access_token, user_id, created_at, end_date_at",
 		accessToken,
@@ -50,4 +51,40 @@ func (t *Token) Validate(token *grpc.Token) bool {
 		return false
 	}
 	return true
+}
+
+func (t *Token) Block(accessToken string) (string, error) {
+	var token string
+	if err := t.db.Pool.QueryRow("UPDATE users SET end_date_at = $1 "+
+		"where user.user_id = $2 RETURNING access_token",
+		time.Now(),
+		accessToken,
+	).Scan(&token); err != nil {
+		return "", err
+	}
+	return token, nil
+}
+
+func (e *Token) GetList(userID int64) ([]model.Token, error) {
+	tokens := []model.Token{}
+	rows, err := e.db.Pool.Query("SELECT access_token, user_id, created_at, end_date_at FROM token "+
+		"where user_id = $1 and end_date_at IS NULL",
+		userID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return tokens, errors.ErrRecordNotFound
+		} else {
+			return tokens, err
+		}
+	}
+	defer rows.Close()
+	for rows.Next() {
+		token := model.Token{}
+		err = rows.Scan(&token.AccessToken, &token.UserID, &token.CreatedAt, &token.EndDateAt)
+		if err != nil {
+			return tokens, err
+		}
+		tokens = append(tokens, token)
+	}
+	return tokens, nil
 }
